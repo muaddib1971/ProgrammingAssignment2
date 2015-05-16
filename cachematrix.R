de4815acac5691262d329093a83f71e94e4a0e23
Paul Miller <paul.miller@rmit.edu.au>
2015-05-16 18:39:04 +1000

    started on assignment 2 although I am concerned about getting this to work. It seems like a poorly formulated assignment to me.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<<-NULL
+  set <- function (y)
+  {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function() m <<-solve
+  getsolve <-function() m
+  list(set = set, get = get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
@@ -12,4 +21,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  if(!is.recursive(x)) print ("no")
+  m<-x$getsolve()
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return (m)
+  }
+  data<-x$get()
+  m<-solve(data, ...)
+  x$setsolve(m)
+  m
 }
